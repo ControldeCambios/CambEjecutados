26f20773707e1a4a8dffdba70cc525fbba7cf4bd
ControldeCambios <53344816+ControldeCambios@users.noreply.github.com>
2019-10-17 10:22:29 -0500

    Add files via upload

---
+++ CCAP030 - 17-07-2019 Visualizacion Bitacora Pagos Int/Fuentes/Script/2. script canal29.sql
@@ -0,0 +1,4 @@
+insert into CANAL29.tsistransacciones values('04', '0030', 'CARGA DE ARCHIVO SPI 4', 1, 0);
+insert into CANAL29.tsismenutransacciones values('EST', '04', '0030');
+INSERT INTO CANAL29.TSISTRANSACCIONROL (CMODULO, CTRANSACCION, CROL, HORADESDE, HORAHASTA) VALUES
+('04', '0030', '000', '0000', '2359');
